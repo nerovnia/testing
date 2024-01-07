2c463834c515ac69642465e838114843ac000b08
Volodymyr Nerovnia <nerv@i.ua>
2024-01-08 01:16:44 +0200

    Coomplete task 3 basic JS 2024 ukr course (Timur Shemsedinov)

---
+++ Metarhia/JS_Basic_2024/task_3.js
@@ -0,0 +1,14 @@
+const purchase = {
+  Electrinix: [
+    { name: 'laptop', price: 1500},
+    { name: 'keyboard', price: 100},
+    [name 'usb-stick'], price: 6
+  ],
+  Textile: [
+    { name: 'bag', price: 50},
+    { name: 'bag', price: 50},    
+  ]
+}
+
+
+console.dir(purchase);
