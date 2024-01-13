1e24e582045f74f6ad1ffd367d66d11fe74db60b
Volodymyr Nerovnia <nerv@i.ua>
2024-01-13 04:52:44 +0200

    Added buble sort to algoritms

---
+++ Algorithms/sorts/bulbe.js
@@ -0,0 +1,19 @@
+const arr1 = [3,5,8,5,4,8,5,8,95,4,55,4,6,34,75,456,7867,54,4];
+
+const sort = (arr) => {
+  let changed = false;
+  for (let i = 0; i < arr.length; i++) {
+    if(i < arr.length && arr[i] > arr[i + 1]) {
+      arr[i] += arr[i + 1];
+      arr[i + 1] = arr[i] - arr[i + 1];
+      arr[i] -= arr[i + 1];
+      changed = true;
+    }
+  }
+  if (changed) {
+    sort(arr);
+  }
+  return [...arr]
+}
+
+console.log(sort([...arr1]));
