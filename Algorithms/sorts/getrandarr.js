6154c90bfd9af53609a60a4d04ea3a44400d2a1d
Volodymyr Nerovnia <nerv@i.ua>
2024-01-16 21:53:16 +0200

    Added array generator with random items

---
+++ Algorithms/sorts/getrandarr.js
@@ -0,0 +1,9 @@
+const getrandarr = (arrlength, minvalue, maxvalue) => {
+  const resarr = [];
+  for (let i = 0; i < arrlength; i++) {
+    resarr.push(Math.floor(Math.random() * (maxvalue - minvalue)) + minvalue);
+  }
+  return [...resarr];
+}
+
+module.exports = { getrandarr };
