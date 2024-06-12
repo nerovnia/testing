b3cc422250c6ee1ef370db7d6320a4d7ac3cf7fc
Volodymyr Nerovnia <nerv@i.ua>
2024-06-12 23:59:49 +0300

    Added heving Abort controller

---
+++ Async/AbortController/having-ac.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const havingAbortController = async () => {
+  const controller = new AbortController();
+  const { signal } = controller;
+
+  const timer = setTimeout(() => {
+    controller.abort();
+  }, 1000);
+
+  const url = "https://developer.mozilla.org/";
+  const response = await fetch(url, { signal });
+  clearTimeout(timer);
+  console.log(`fetch ${url} status ${response.status}`);
+};
+
+havingAbortController();
